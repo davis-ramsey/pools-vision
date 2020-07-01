c6352600fefc3463dd5749a2daa94d2bb61e5661
solarcurve1 <solarcurve1@users.noreply.github.com>
2020-07-01 17:09:05 -0400

    button to multiple portfolios working

--- src/reducers/portfolioReducer.js
+++ src/reducers/portfolioReducer.js
@@ -3,7 +3,9 @@ const INITIAL_STATE = [];
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
 		case 'SELECT_POOL':
-			return action.payload;
+			const newState = state.map((value) => value);
+			newState.push(action.payload);
+			return newState;
 		default:
 			return state;
 	}
