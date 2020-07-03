19df3cd32ecdc1bd3a85d0f0c74a8800a4652f27
Davis Ramsey <davis.ramsey@gmail.com>
2020-07-02 22:42:04 -0400

    fixed up the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,32 @@ class Header extends React.Component {
 	render() {
 		if (this.props.portfolio.length !== 0)
 			return (
-				<div className="two ui buttons header">
+				<div className="five ui buttons header">
 					<Link
 						to={`/portfolio/${this.props.portfolio.filter((value) => value !== '' && value !== null)}`}
 						className="ui button left aligned primary"
 					>
 						Portfolio
 					</Link>
+					<div className="ui animated secondary button">
+						<div className="ui avatar image visible content">
+							<img alt="grug stack rock" src="/images/pebbles.png" />
+						</div>
+						<div className="hidden content white">Grug stack rock</div>
+					</div>
+
+					<div className="ui animated secondary button">
+						<div className="ui avatar image visible content">
+							<img alt="graph" src="/images/graph.png" />
+						</div>
+						<div className="hidden content">Built using The Graph Protocol</div>
+					</div>
+					<div className="ui animated secondary button">
+						<div className="ui avatar image visible content">
+							<img alt="coingecko" src="/images/gecko.png" />
+						</div>
+						<div className="hidden content">Powered by CoinGecko API</div>
+					</div>
 					<Link to="/" className="ui button right aligned primary">
 						All Pools
 					</Link>
@@ -20,8 +39,27 @@ class Header extends React.Component {
 			);
 		else
 			return (
-				<div className="two ui buttons header">
-					<Link to="/" className="ui button right aligned primary">
+				<div className="five ui buttons header">
+					<div className="ui button left aligned primary loading">Portfolio</div>
+					<div className="ui animated secondary button ">
+						<div className="ui avatar image visible content">
+							<img alt="grug stack rock" src="/images/pebbles.png" />
+						</div>
+						<div className="hidden content">Grug stack rock</div>
+					</div>
+					<div className="ui animated secondary button ">
+						<div className="ui avatar image visible content">
+							<img alt="graph" src="/images/graph.png" />
+						</div>
+						<div className="hidden content">Built using The Graph Protocol</div>
+					</div>
+					<div className="ui animated secondary button ">
+						<div className="ui avatar image visible content">
+							<img alt="coingecko" src="/images/gecko.png" />
+						</div>
+						<div className="hidden content">Powered by CoinGecko API</div>
+					</div>
+					<Link to="/" className="ui button right aligned primary ">
 						All Pools
 					</Link>
 				</div>
