6f851e0b5dcf4324af7b8c8775108f1dd7c943b6
Davis Ramsey <davis.ramsey@gmail.com>
2020-07-04 14:58:41 -0400

    Updates

---
+++ precache-manifest.5d828b504a9594e5b70c750b3cc03f2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea6f7a802bd61f418b4d327fbc5f530",
+    "url": "/pools-vision/index.html"
+  },
+  {
+    "revision": "68331aabf8a18d5762a6",
+    "url": "/pools-vision/static/js/2.f08633e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pools-vision/static/js/2.f08633e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1cd34ac0847e8612e18",
+    "url": "/pools-vision/static/js/main.b4a0856e.chunk.js"
+  },
+  {
+    "revision": "ec2a32edf69cab5d86ff",
+    "url": "/pools-vision/static/js/runtime-main.e7939d5c.js"
+  }
+]);
