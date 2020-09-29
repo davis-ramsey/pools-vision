7b76b93235aa6f9983bd6c03e839747eda3c34cb
Davis Ramsey <davis.ramsey@gmail.com>
2020-09-29 11:10:46 -0400

    update error message

--- src/reducers/errorReducer.js
+++ src/reducers/errorReducer.js
@@ -1,5 +1,5 @@
 const INITIAL_STATE =
-	'Gathering data from the balancer subgraph! This may take a few seconds. Data will automatically refresh every 5 minutes.';
+	'Gathering data from the balancer subgraph! This may take a few seconds. Data will automatically refresh in the background every 5 minutes.';
 
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
